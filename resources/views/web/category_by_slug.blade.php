6c6568946879a88d6dc1139d4b4750ad96058ea0
Bayu <bayurahmatn0@gmail.com>
2025-04-16 13:17:39 +0700

    update web terbaru

--- resources/views/web/category_by_slug.blade.php
+++ resources/views/web/category_by_slug.blade.php
@@ -1,9 +1,9 @@
-<x-layout>
+<<x-layout>
     @if($category)
         <h1>{{ $category['name'] }}</h1>
         <hr>
         <p>{{ $category['description'] }}</p>
     @else
-        <p class="text-danger">Kategori tidak ditemukan.</p>
+        <p class="text-muted">Oops! Kategori yang Anda cari tidak ditemukan.</p>
     @endif
 </x-layout>
